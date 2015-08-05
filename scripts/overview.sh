22f9ae12f9d22bee6b8db0c445e7cf1d533041f8
Ondrej Zara <ondrej.zara@gmail.com>
2015-08-05 22:24:25 +0200

    overview datetime

--- scripts/overview.sh
+++ scripts/overview.sh
@@ -2,4 +2,4 @@
 
 # print a gps data summary
 
-exiftool -filename -gpslatitude -gpslongitude -gpsaltitude -T -n "$@"
+exiftool -filename -gpslatitude -gpslongitude -gpsaltitude -gpsdatetime -T -n "$@"
